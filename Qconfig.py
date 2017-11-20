5bbd9f8994afd49fb6c9a0f2ad80bf31735174e4
yasster <ymissoum1@gmail.com>
2017-11-20 14:01:19 -0800

    Update Qconfig.py

--- Qconfig.py
+++ Qconfig.py
@@ -3,7 +3,7 @@
 # access token. Replace "None" below with the quoted token string.
 # Uncomment the APItoken variable, and you will be ready to go.
 
-APItoken = "b899b6b61a9f873fa9e7cfd638a05c20621faab6f9d68f48dfe34a0c69d277137e1edeea270b18a3bf9c94e43599fad93a0022e24274ef296fa65ba9e489659a"
+APItoken = "API Key"
 
 config = {
   "url": 'https://quantumexperience.ng.bluemix.net/api'
